aa3fbb810a56cc5f878323c0a9ad4a4a5b1c00c9
rraihan <dialraihan@gmail.com>
2018-06-16 11:35:50 -0400

    First file Lodded

--- src/chapter05/ForLoopExample.java
+++ src/chapter05/ForLoopExample.java
@@ -3,7 +3,11 @@ package chapter05;
 public class ForLoopExample {
     public static void main(String args[]){
          for(int i=10; i>1; i--){
-              System.out.println("The value of i is: "+i);
-         }
+        	 for(int r=0; r<=1; r++)
+              //System.out.println("The value of i is: "+i);
+              System.out.println( i + "-" + r +"=" + i);
+            
+         
     }
+}
 }
